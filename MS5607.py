96e9beb8d693dde1f91344a1d87c4d558cd28dec
Paul Sheridan <paul.sheridan@soartech.com>
2021-04-21 10:29:38 -0400

    change address and bus number from hard coded to parameters

--- MS5607.py
+++ MS5607.py
@@ -2,7 +2,7 @@ import time
 import smbus
 import math
 
-bus = smbus.SMBus(1)
+#bus = smbus.SMBus(1)
 
 class MS5607:
     """
@@ -10,7 +10,7 @@ class MS5607:
     http://www.parallaxinc.com/sites/default/files/downloads/29124-MS5607-02BA03-Datasheet.pdf
     Offset for humidity to provide better precision
     """
-    DEVICE_ADDRESS = 0x76
+    #DEVICE_ADDRESS = 0x76
     _CMD_RESET = 0x1E
     _CMD_ADC_READ = 0x00
     _CMD_PROM_RD = 0xA0
@@ -23,16 +23,19 @@ class MS5607:
     _CMD_ADC_2048 = 0x06
     _CMD_ADC_4096 = 0x08
 
-    def __init__(self):
+    def __init__(self, bus_num = 1, address = 0x76):
+        self.bus_num = bus_num
+        self.address = address
+        self.bus = smbus.SMBUS(bus_num)
         self.resetSensor()
         self.coefficients = self.readCoefficients()
 
     # Some utility methods
     def read16U(self, register1, register2):
-        bytes = bus.read_i2c_block_data(self.DEVICE_ADDRESS, register1, 2)
+        bytes = self.bus.read_i2c_block_data(self.address, register1, 2)
         return (bytes[0] << 8) + (bytes[1])
     def read24U(self, register):
-        bytes = bus.read_i2c_block_data(self.DEVICE_ADDRESS, register, 3)
+        bytes = self.bus.read_i2c_block_data(self.adress, register, 3)
         return (bytes[0] << 16) + (bytes[1] << 8) + bytes[2]
     def hectoPascalToInHg(self, milliBar):
         return milliBar * 29.5333727 / 100000
@@ -45,7 +48,7 @@ class MS5607:
 
     # Commands		
     def resetSensor(self):
-        bus.write_byte(self.DEVICE_ADDRESS, self._CMD_RESET)
+        self.bus.write_byte(self.adress, self._CMD_RESET)
         time.sleep(0.003) # wait for the reset sequence timing
     def readCoefficient(self, i):
         return self.read16U(self._CMD_PROM_RD + 2 * i, self._CMD_PROM_RD + 2 * i + 1)
@@ -56,7 +59,7 @@ class MS5607:
         return coefficients
     def readAdc(self, cmd):
         # set conversion mode
-        bus.write_byte(self.DEVICE_ADDRESS, self._CMD_ADC_CONV + cmd)
+        self.bus.write_byte(self.adress, self._CMD_ADC_CONV + cmd)
         sleepTime = {self._CMD_ADC_256: 0.0009, self._CMD_ADC_512: 0.003, self._CMD_ADC_1024: 0.004, self._CMD_ADC_2048: 0.006, self._CMD_ADC_4096: 0.010}
         time.sleep(sleepTime[cmd & 0x0f])
         return self.read24U(self._CMD_ADC_READ)
